afb84f3470f669b228216f946b668a7dd75d0941
Dennis Weddig <dennis.weddig@mpls.frb.org>
2020-10-13 14:33:37 -0500

    fix for my install

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
+module "s3-webapp-dennisw" {
+  source  = "app.terraform.io/weddig_enterprises/s3-webapp-dennisw/aws"
   version = "1.0.0"
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
 }
